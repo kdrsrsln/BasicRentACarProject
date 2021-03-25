06bc9aa7ef983d0333fc330f27cb855d67848ec4
Kadir Sarıaslan <software@sariaslan.net>
2021-03-25 14:48:04 +0300

    Initialized WebAPI

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
